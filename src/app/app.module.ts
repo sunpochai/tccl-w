426030ba44fcaaa4c3dcef1a6f9b365b03fcbb4c
sanchai <sunpochai@gmail.com>
2017-12-24 10:04:43 +0700

    add init

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ThemeComponent } from './theme/theme.component';
+import { LayoutModule } from './theme/layouts/layout.module';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ScriptLoaderService } from "./_services/script-loader.service";
+import { ThemeRoutingModule } from "./theme/theme-routing.module";
+import { AuthModule } from "./auth/auth.module";
+import { DashboardComponent } from './tccl/pages/dashboard/dashboard.component';
+
+@NgModule({
+    declarations: [
+        ThemeComponent,
+        AppComponent,
+        DashboardComponent,
+    ],
+    imports: [
+        LayoutModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        ThemeRoutingModule,
+        AuthModule,
+    ],
+    providers: [ScriptLoaderService],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
