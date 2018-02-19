3bcf5415c284696feaeae663ac6446f388d0e4e3
Sanchai Pochai <SanchaiP@Sanchais-MacBook-Pro.local>
2018-02-19 21:52:07 +0700

    add doctype api

--- src/app/app-constants.ts
+++ src/app/app-constants.ts
@@ -1,7 +1,10 @@
 export const API_BASE_URL: string = 'http://sunpochai.thddns.net:5555'
-export const API_TRACKING_LIST: string = API_BASE_URL + '/metronic/preview/inc/api/datatables/demos/default.php'
 export const API_COMPANY_LIST: string = API_BASE_URL + '/api/company/list'
 export const API_COMPANY_GET: string = API_BASE_URL + '/api/company'
 export const API_COMPANY_PUT: string = API_BASE_URL + '/api/company'
 export const API_COMPANY_DEL: string = API_BASE_URL + '/api/company'
 export const API_COMPANY_INSERT: string = API_BASE_URL + '/api/company/insert' 
+
+export const API_DOCTYPE_LIST: string = API_BASE_URL + '/api/doctype/list'
+export const API_DOCTYPE_GET_PUT_DEL: string = API_BASE_URL + '/api/doctype'
+export const API_DOCTYPE_INSERT: string = API_BASE_URL + '/api/doctype/insert'  
