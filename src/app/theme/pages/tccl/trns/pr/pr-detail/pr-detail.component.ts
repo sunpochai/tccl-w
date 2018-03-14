2662338c1869be0f4948a2f56bd1917459fba425
krataiinoi <krataiinoi@gmail.com>
2018-03-14 22:35:34 +0700

    -

--- src/app/theme/pages/tccl/trns/pr/pr-detail/pr-detail.component.ts
+++ src/app/theme/pages/tccl/trns/pr/pr-detail/pr-detail.component.ts
@@ -39,6 +39,11 @@ export class PRDetailComponent extends PageBaseComponent implements OnInit, Afte
     public docStatus: Array<any> = C_DOC_STATUS_2;
     public attFile :any ;
     public formData: FormData = new FormData(); 
+    public fileList: FileList;
+    public action_attach_file_id: any;
+    public action_file_name: any;
+    public action_type: any;
+    public action_comment: any;
     constructor(  
         private _script: ScriptLoaderService,
         private _router: Router, 
@@ -78,13 +83,12 @@ export class PRDetailComponent extends PageBaseComponent implements OnInit, Afte
                     }
                 }
                 super.unblockui('#m-content');
-                console.log(this.pr);
+                // console.log(this.pr);
             },
             error => {
                 super.showError(error);
                 console.log('error');
                 super.unblockui('#m-content');
-    
             },
             () => {
                 super.unblockui('#m-content');
@@ -105,25 +109,27 @@ export class PRDetailComponent extends PageBaseComponent implements OnInit, Afte
             ['assets/tccl/trns/pr/pr-detail.js']);
     }
 
+    prepareRemoveFile(attachId, fileName) {
+        this.action_attach_file_id = attachId;
+        this.action_file_name = fileName;
+    }
     
-    removeFile(attachId, fileIndex) {
+    removeFile() {
         // alert(attachId + ',' + fileIndex);
         super.blockui('#m-content');
 
-        this._attachmentService.del(attachId).subscribe(resp => {
-                super.unblockui('#m-content');
-                super.showsuccess('Remove file complete');
-                //todo:: refresh file list
-                this.pr.pr_attachment_items.forEach( (item, index) => {
-                    if(item.attach_id === attachId) this.pr.pr_attachment_items.splice(index,1);
-                });
-             
+        this._attachmentService.del(this.action_attach_file_id).subscribe(resp => {
+            super.unblockui('#m-content');
+            super.showsuccess('Remove file complete');
+            //todo:: refresh file list
+            this.pr.pr_attachment_items.forEach( (item, index) => {
+                if(item.attach_id === this.action_attach_file_id) this.pr.pr_attachment_items.splice(index,1);
+            });  
         },
         error => {
             super.showError(error);
             console.log('error');
             super.unblockui('#m-content');  
-
         },
         () => {
             super.unblockui('#m-content');
@@ -134,6 +140,11 @@ export class PRDetailComponent extends PageBaseComponent implements OnInit, Afte
 
     openFile(fileId) {
         window.open(API_ATTACHMENT_GET_DEL + '/' + fileId);
+    }
+
+    prepareAction(action) {
+        this.action_type = action;
+        this.action_comment = $('#txtComment').val().toString();
     }
     
     review() {
@@ -146,6 +157,8 @@ export class PRDetailComponent extends PageBaseComponent implements OnInit, Afte
         workflowaction.actor_username = super.getFullNameUserLogin();
         workflowaction.outcome = STATUS_NAME.reviewed;
         workflowaction.outcome_description = $('#txtComment').val().toString();
+
+        console.log(workflowaction);
 
         this._workflowService.review<any>(workflowaction).subscribe(
             resp => {
@@ -179,7 +192,7 @@ export class PRDetailComponent extends PageBaseComponent implements OnInit, Afte
         workflowaction.wf_stage_resp_id = this.wf_stage_resp_id;
         workflowaction.actor_user = super.getADUserLogin();
         workflowaction.actor_username = super.getFullNameUserLogin();
-        workflowaction.outcome = STATUS_NAME.approved;// C_DOC_STATUS_APPROVED_NAME;
+        workflowaction.outcome = STATUS_NAME.approved;
         workflowaction.outcome_description = $('#txtComment').val().toString();
 
         console.log(workflowaction);
@@ -214,7 +227,7 @@ export class PRDetailComponent extends PageBaseComponent implements OnInit, Afte
         workflowaction.wf_stage_resp_id = this.wf_stage_resp_id;
         workflowaction.actor_user = super.getADUserLogin();
         workflowaction.actor_username = super.getFullNameUserLogin();
-        workflowaction.outcome = STATUS_NAME.rejected //C_DOC_STATUS_REJECTED_NAME;
+        workflowaction.outcome = STATUS_NAME.rejected
         workflowaction.outcome_description = $('#txtComment').val().toString();
 
         this._workflowService.reject<any>(workflowaction).subscribe(
@@ -251,27 +264,21 @@ export class PRDetailComponent extends PageBaseComponent implements OnInit, Afte
     }
 
     performAction() {
-        var pAction =  $('#input_action').val().toString().toLocaleLowerCase();
-
-        switch (pAction) {
+// console.log(this.action_type.toLowerCase().toString());
+        switch (this.action_type.toLowerCase().toString()) {
             case 'review':
-                // console.log('doaction review');
                 this.review();
                 break;
             case 'approve':
-                // console.log('doaction approve');
                 this.approve();
                 break;
             case 'reject':
-                // console.log('doaction reject');
                 this.reject();
                 break;
             case 'waiting':
-                // console.log('doaction waiting');
                 this.waiting();
                 break;
             case 'comment':
-                // console.log('doaction waiting');
                 this.comment();
                 break;
         }
@@ -280,56 +287,62 @@ export class PRDetailComponent extends PageBaseComponent implements OnInit, Afte
     navigate_list() {
         this._router.navigate(['/trns/pr/list']);
     }
-
    
-fileChange(event) {  
-    //ebugger;  
- 
-    let fileList: FileList = event.target.files;  
-    if (fileList.length > 0) { 
-        this.attFile = []; 
-        let headers = new Headers()  
-            //headers.append('Content-Type', 'json');  
-            //headers.append('Accept', 'application/json');  
+    fileChange(event) {
+        //ebugger;  
+        this.fileList = event.target.files;  
+        // console.log(this.fileList);
+
+        if (this.fileList.length > 0) { 
+            this.attFile = []; 
+            
+            for (let index = 0; index < this.fileList.length; index++) {
+                let file = this.fileList[index];
+                this.attFile.push(file.name);
+            }
+            // console.log(this.attFile);
+        } else {
+            this.attFile = null;
+        }
+    }
+
+    uploadFile() {
+        super.blockui('#m-content');
+
+        if (this.fileList.length > 0) { 
             this.formData.append("doc_group",ROUTE_PR.doc_group);   
             this.formData.append("doc_id",this.pr.pr_id.toString());  
             this.formData.append("create_user",this.getADUserLogin());  
             this.formData.append("create_username",this.getFullNameUserLogin());
-         
             
-        for (let index = 0; index < fileList.length; index++) {
-            let file = fileList[index];
-            this.formData.append("file_" + index.toString(), file, file.name); 
-            this.attFile.push(file.name);
-        } 
-          
-    }else{
-        this.attFile = null;
-    }
-}
-uploadFile(){
-    super.blockui('#m-content');
-    
+            for (let index = 0; index < this.fileList.length; index++) {
+                let file = this.fileList[index];
+                this.formData.append("file_" + index.toString(), file, file.name); 
+            }
+            console.log(this.attFile);
+        } else {
+            // this.attFile = null;
+        }
+
         this._attachmentService.upload(this.formData).subscribe(  
             data => {
                 let att  = data;
-                console.log(data);  
+                console.log(att);  
                 this.attFile = null;
                 this.formData = new FormData()  ;
                 super.unblockui('#m-content'); 
                 super.showsuccess('upload complete');
                 this.pr.pr_attachment_items = this.pr.pr_attachment_items || [] ;
-                 for (let index = 0; index < att.length; index++) {
-                        this.pr.pr_attachment_items.push(att[index]);
+
+                for (let index = 0; index < att.length; index++) {
+                    this.pr.pr_attachment_items.push(att[index]);
                 }
-                
+                console.log(this.pr.pr_attachment_items);
             },  
-            error => {  
-                    
+            error => {
                 super.unblockui('#m-content');
                 super.showError(error);
             }
-            );    
-        
-}
+        );
+    }
 }
