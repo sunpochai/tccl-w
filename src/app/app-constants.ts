532d107cf381c132596baeca77a250819ab0b495
sunpochai@gmail.com <sunpochai@gmail.com>
2018-08-20 19:41:20 +0700

    00

--- src/app/app-constants.ts
+++ src/app/app-constants.ts
@@ -1,14 +1,14 @@
 //Service URL 
 //export const API_BASE_URL: string =  'https://portal.tccland.com:8090/OASRESTApi'
 //export const API_BASE_URL: string = 'http://202.60.207.137/WebRESTApi'
-export const API_BASE_URL: string = 'http://sunpochai.thddns.net:5555'
-//export const API_BASE_URL: string = 'http://localhost:65179/' 
+//export const API_BASE_URL: string = 'http://sunpochai.thddns.net:5555'
+export const API_BASE_URL: string = 'http://localhost:65179/' 
 //export const API_BASE_URL: string = 'http://192.168.1.12:5555'     
 export const API_AUTHEN_TOKEN: string = API_BASE_URL + '/token' 
 export const API_AUTHEN_CHECKIN: string = API_BASE_URL + '/api/authen/checkin'
 export const API_AUTHEN_CHECKOUT: string = API_BASE_URL + '/api/authen/checkout'
 export const API_AUTHEN_VERIFY: string = API_BASE_URL + '/api/authen/wverify'
-   
+     
 export const API_COMPANY_GETALL: string = API_BASE_URL + '/api/company/getall' //list all item (use with dropdown)
 export const API_COMPANY_LIST: string = API_BASE_URL + '/api/company/list' //search item (use with search page)
 export const API_COMPANY_GET: string = API_BASE_URL + '/api/company'
